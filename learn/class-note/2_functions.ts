417eada54082055a4fbd019e174f172568bf6ff7
kimjinsoo <1004kjs_@naver.com>
2021-08-09 23:30:38 +0900

    typescript function 학습 완료

---
+++ learn/class-note/2_functions.ts
@@ -0,0 +1,26 @@
+// 함수의 파라미터에 타입을 정의하는 방식
+function sum1(a: number, b: number) {
+    return a + b;
+}
+
+sum1(10, 20)
+
+// 함수의 반환 값에 타입을 정의하는 방식
+function add(): number {
+    return 10;
+}
+
+// 함수의 타입을 종합적으로 정의하는 방식
+function sum2(a: number, b: number): number {
+    return a + b;
+}
+
+sum(10, 20);
+
+// 함수의 옵셔널 파라미터 => ?
+function log(a: string, b?: string) {
+
+}
+
+log('hello world');
+log('hello ts', 'abc')
---
+++ learn/class-note/function.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(10, 20, 30, 40, 50));
