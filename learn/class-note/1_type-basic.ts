3fe87030f43b1e7109da388d1e2c6bc33457c146
kimjinsoo <1004kjs_@naver.com>
2021-08-09 23:04:19 +0900

    typescript 타입 공부

---
+++ learn/class-note/1_type-basic.ts
@@ -0,0 +1,32 @@
+// 문자열
+// js 문자열 선언
+var str1 = "hello";
+var num1 = 10
+let arr1 = [1, 2, 3]
+// ts 문자열 선언
+let str2: string = "hello"
+
+// ts 숫자
+let num2: number = 10;
+
+// ts 배열
+let arr2: Array<number> = [1, 2, 3];
+let heroes : Array<string> = ["jinsoo"];
+let items: number[] = [1,2,3];
+
+// ts 튜플
+let address: [string, number] = ["gangnam", 1];
+
+// ts 객체
+let obj: object = {};
+// let person: object = {
+//     name: 'jinsoo',
+//     age: 23
+// };
+let person: { name: string, age: number} = {
+    name: "jinsoo",
+    age: 23
+}
+
+// ts 진위값
+let show: boolean = true;
