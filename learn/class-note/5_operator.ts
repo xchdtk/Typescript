5c13bbc57b3d5d7a04920131a5b6797e95b83d50
kimjinsoo <1004kjs_@naver.com>
2021-09-22 22:44:26 +0900

    유니온타입, 인터섹션타입 학습

---
+++ learn/class-note/5_operator.ts
@@ -0,0 +1,56 @@
+// function logMessage(value: any) {
+//     console.log(value);
+// }
+
+// logMessage('hello');
+// logMessage(100);
+// logMessage(false);
+
+
+// 유니온 타입(|)
+let jinsoo: string | number | boolean
+function logMessage(value: string | number) {
+    if (typeof value === 'number') {
+        value.toLocaleString();
+    }
+    if (typeof value === 'string') {
+        value.toString();
+    }
+    throw new TypeError('value must be string or number')
+}
+
+logMessage('hello');
+logMessage(100);
+
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+interface Person {
+    name: string;
+    age: number;
+}
+// 유니온 타입(|)은 공통된 속성만 접근이 가능하다.
+// function askSomeone(someone: Developer | Person) {
+//     // console.log(someone.name)
+// }
+
+// askSomeone({ name: '디벨로퍼', skill: "웹개발"});
+// askSomeone({ name: "캡틴", age: 100});
+
+
+// 인터섹션 타입
+function askSomeone(someone: Developer & Person) {
+    someone.name;
+    someone.skill;
+    someone.age;
+}
+
+askSomeone({ name: '디벨로퍼', skill: "웹개발", age: 100});
+askSomeone({ name: "캡틴", age: 100, skill: "앱개발"});
+
+// let seho: string | number | boolean;
+// let capt: string & number & boolean;
+
+ 
